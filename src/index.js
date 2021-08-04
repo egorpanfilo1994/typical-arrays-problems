d63779024186028b2020a37824f3fab5b0b45b28
egorpanfilo1994 <egorpanfilo1994@gmail.com>
2021-08-04 15:20:19 +0300

    feat: task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+exports.min = function min(array) {
+    if (!array || array.length === 0) {
+        return 0
+    };
+    let min = array[0];
+    for (let i = 0; i < array.length; i++) {
 
         if (min > array[i]) min = array[i];
     }
