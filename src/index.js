027122363efbd8257e2fb859195720a03a399f23
egorpanfilo1994 <egorpanfilo1994@gmail.com>
2021-08-04 15:19:04 +0300

    feat: task solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
-exports.min = function min (array) {
-  return 0;
+        if (min > array[i]) min = array[i];
+    }
+
+    return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!array || array.length === 0) {
+        return 0
+    };
+    let max = array[0];
+    for (let i = 0; i < array.length; i++) {
+
+        if (max < array[i]) max = array[i];
+    }
+
+    return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!array || array.length === 0) {
+        return 0
+    };
+    return array.reduce((a, b) => (a + b)) / array.length;
 }
